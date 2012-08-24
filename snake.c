f72df9e9fe13f26e9beff1c4ad29b0c53fe4d7f6
Pedro Mariano <pedroh.mariano@gmail.com>
2012-08-23 23:36:57 -0300

    feeding the snake

--- snake.c
+++ snake.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <SDL/SDL.h>
 
 #define SCREEN_WIDTH  1024
 #define SCREEN_HEIGHT 768
 #define FPS 30
 
-
 #define UP 1
 #define DOWN 2
 #define LEFT 3
 #define RIGHT 4
 
-SDL_Surface *screen, *head_img, *tail_img;
+SDL_Surface *screen, *head_img, *tail_img, *food_img;
 int num_tail_img = 1;
 // 1 up 2 down 3 left 4 right
 int orientation = 4;
 int vel = 2;
 int bpi = 15;
 int pieces_qtd = 1;
+int food_qtd = 0;
 SDL_Rect pieces[50000] = {};
+SDL_Rect food ;
 
 void move(){
 	int j , sumx, sumy;
@@ -46,7 +49,6 @@ void move(){
 		SDL_BlitSurface(head_img, NULL, screen, piece);
 	  } else {
 		SDL_Rect before = pieces[j-1];
-		printf("BEFORE POS X: %i Y: %i \n", before.x, before.y);
 		piece->x = before.x;
 		piece->y = before.y;
 		SDL_BlitSurface(tail_img, NULL, screen, piece);
@@ -54,7 +56,7 @@ void move(){
 	}
 }
 
-void add_a_piece(){
+void add_piece(){
 	int len = sizeof(pieces)/sizeof(SDL_Rect);
 	int x = pieces[pieces_qtd - 1].x;
 	int y = pieces[pieces_qtd - 1].y;
@@ -64,6 +66,43 @@ void add_a_piece(){
 	next_piece->y = y;
 	pieces_qtd++;
 }
+
+int generate_random(int min, int max){
+  int r = (rand() + max + min) % max ;
+
+  //can I remove this if?
+  if(r < min ){
+	r += min;
+  }
+
+  return r;
+}
+
+void draw_food(){
+  if(food_qtd == 0){
+	food.x = generate_random(15,SCREEN_WIDTH);
+	food.y = generate_random(15,SCREEN_HEIGHT);
+
+	food_qtd++;
+  }
+
+  SDL_BlitSurface(food_img, NULL, screen, &food);
+}
+
+
+void handle_colision(){
+  SDL_Rect head = pieces[0];
+  if(food_qtd > 0){
+	int xdiff = abs(head.x - food.x);
+	int ydiff = abs(head.y - food.y);
+	if(xdiff <= 15 && ydiff <= 15){
+	  printf("RONALDOOOO");
+	  food_qtd--;
+	  add_piece();
+	}
+  }
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -75,14 +114,15 @@ int main(int argc, char* argv[])
     screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 0, SDL_HWSURFACE);
 	head_img = SDL_LoadBMP("cube_red.bmp");
 	tail_img = SDL_LoadBMP("cube_green.bmp");
+	food_img = SDL_LoadBMP("cube_yellow.bmp");
 
-	Uint32 last_update = SDL_GetTicks();
 	SDL_Event event;
 	int count;
 	int exit = 0;
 
 	while(!exit){
 	  count++;
+	  Uint32 start_time= SDL_GetTicks();
 
 	  if(SDL_PollEvent(&event)){
 		switch(event.type) {
@@ -99,7 +139,7 @@ int main(int argc, char* argv[])
 				vel *= 2;
 				break;
 			  case SDLK_p:
-				add_a_piece();
+				add_piece();
 				break;
 			  case SDLK_MINUS:
 				vel /= 2;
@@ -129,19 +169,28 @@ int main(int argc, char* argv[])
 		}
 	  }
 
-	  Uint32 current_time = SDL_GetTicks();
 	  //arredondamento fail
-	  if((current_time - last_update) >= 1000/FPS){
-		SDL_Delay(1000/FPS);
-		SDL_FillRect(screen , NULL , 0x00000000);
-		move();
-		last_update = current_time;
-		SDL_UpdateRect(screen, 0, 0, 0, 0);
+	  SDL_FillRect(screen , NULL , 0x00000000);
+	  move();
+	  handle_colision();
+	  draw_food();
+
+	  SDL_UpdateRect(screen, 0, 0, 0, 0);
+
+	  Uint32 current_time = SDL_GetTicks();
+	  int expected_time =  1000/FPS;
+	  unsigned int diff = current_time - start_time;
+
+	  printf("DELAY: %i\n", expected_time - diff);
+	  int delay = expected_time - diff;
+
+	  if(delay < 0){
+		delay = 0;
 	  }
 
+	  SDL_Delay(delay);
 	}
 
     return 0;
 }
-
 
