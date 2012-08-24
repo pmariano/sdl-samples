3f23ecb6dacbf47c68517e65da7db208998a0aaf
Pedro Mariano <pedroh.mariano@gmail.com>
2012-08-24 00:27:20 -0300

    body collision

--- snake.c
+++ snake.c
@@ -3,9 +3,9 @@
 #include <math.h>
 #include <SDL/SDL.h>
 
-#define SCREEN_WIDTH  1024
-#define SCREEN_HEIGHT 768
-#define FPS 30
+#define SCREEN_WIDTH  800
+#define SCREEN_HEIGHT 600
+#define FPS 20
 
 #define UP 1
 #define DOWN 2
@@ -17,9 +17,10 @@ int num_tail_img = 1;
 // 1 up 2 down 3 left 4 right
 int orientation = 4;
 int vel = 2;
-int bpi = 15;
+int ppi = 15;
 int pieces_qtd = 1;
 int food_qtd = 0;
+int gameover = 0;
 SDL_Rect pieces[50000] = {};
 SDL_Rect food ;
 
@@ -30,19 +31,18 @@ void move(){
 	  SDL_Rect* piece = &pieces[j];
 
 	  if(j == 0){
-		printf("HEAD POS X: %i Y: %i \n", piece->x, piece->y);
 		switch(orientation){
 		  case UP:
-			piece->y -= bpi;
+			piece->y -= ppi;
 			break;
 		  case DOWN:
-			piece->y += bpi;
+			piece->y += ppi;
 			break;
 		  case LEFT:
-			piece->x -= bpi;
+			piece->x -= ppi;
 			break;
 		  case RIGHT:
-			piece->x += bpi;
+			piece->x += ppi;
 			break;
 		 }
 
@@ -68,7 +68,7 @@ void add_piece(){
 }
 
 int generate_random(int min, int max){
-  int r = (rand() + max + min) % max ;
+  int r = rand() % max;
 
   //can I remove this if?
   if(r < min ){
@@ -80,8 +80,8 @@ int generate_random(int min, int max){
 
 void draw_food(){
   if(food_qtd == 0){
-	food.x = generate_random(15,SCREEN_WIDTH);
-	food.y = generate_random(15,SCREEN_HEIGHT);
+	food.x = generate_random(head_img->w,SCREEN_WIDTH);
+	food.y = generate_random(head_img->h,SCREEN_HEIGHT);
 
 	food_qtd++;
   }
@@ -89,17 +89,53 @@ void draw_food(){
   SDL_BlitSurface(food_img, NULL, screen, &food);
 }
 
+int collide(SDL_Rect a, SDL_Rect b, int hit_obj_area){
+	int xdiff = abs(a.x - b.x);
+	int ydiff = abs(a.y - b.y);
 
-void handle_colision(){
-  SDL_Rect head = pieces[0];
+	if(xdiff <= hit_obj_area && ydiff <= hit_obj_area){
+	  return 1;
+	}
+	return 0;
+}
+
+void feeding(SDL_Rect head){
   if(food_qtd > 0){
-	int xdiff = abs(head.x - food.x);
-	int ydiff = abs(head.y - food.y);
-	if(xdiff <= 15 && ydiff <= 15){
+	if(collide(head, food, head_img->w)){
 	  food_qtd--;
 	  add_piece();
 	}
   }
+}
+
+void hit_the_wall(SDL_Rect head){
+  if ( head.x < head_img->w || head.x > (SCREEN_WIDTH - head_img->w)) {
+	gameover = 1;
+  }
+
+  if ( head.y < head_img->h || head.y > (SCREEN_HEIGHT - head_img->h)) {
+	gameover = 1;
+  }
+}
+
+void hit_itself(SDL_Rect head){
+  //start from the first tail
+  int i;
+
+  for(i = 1; i < pieces_qtd; i++){
+	SDL_Rect piece = pieces[i];
+	if(collide(head, piece, head_img->w - 1)){
+	  gameover = 1;
+	  break;
+	}
+  }
+}
+
+void handle_collision(){
+  SDL_Rect head = pieces[0];
+  hit_the_wall(head);
+  hit_itself(head);
+  feeding(head);
 }
 
 
@@ -117,22 +153,26 @@ int main(int argc, char* argv[])
 
 	SDL_Event event;
 	int count;
-	int exit = 0;
 
-	while(!exit){
+	//start point
+	pieces[0].x = SCREEN_WIDTH / 2;
+	pieces[0].y = SCREEN_HEIGHT / 2;
+
+
+	while(!gameover){
 	  count++;
 	  Uint32 start_time= SDL_GetTicks();
 
 	  if(SDL_PollEvent(&event)){
 		switch(event.type) {
 		  case SDL_QUIT:
-			exit = 1;
+			gameover= 1;
 			break;
 		  case SDL_KEYDOWN:
 			switch(event.key.keysym.sym) {
 			  case SDLK_ESCAPE:
 			  case SDLK_q:
-				exit = 1;
+				gameover = 1;
 				break;
 			  case SDLK_EQUALS:
 				vel *= 2;
@@ -171,21 +211,15 @@ int main(int argc, char* argv[])
 	  //arredondamento fail
 	  SDL_FillRect(screen , NULL , 0x00000000);
 	  move();
-	  handle_colision();
+	  handle_collision();
 	  draw_food();
 
 	  SDL_UpdateRect(screen, 0, 0, 0, 0);
 
 	  Uint32 current_time = SDL_GetTicks();
 	  int expected_time =  1000/FPS;
-	  unsigned int diff = current_time - start_time;
-
-	  printf("DELAY: %i\n", expected_time - diff);
-	  int delay = expected_time - diff;
-
-	  if(delay < 0){
-		delay = 0;
-	  }
+	  int diff = current_time - start_time;
+	  int delay = abs(expected_time - diff);
 
 	  SDL_Delay(delay);
 	}
